2832e0190ca3fb70c374ee9b8b55f2649369e167
hudzzz01 <hdzzzzzz01@gmail.com>
2024-03-27 17:48:41 +0700

    build frontend zakat

---
+++ dist/js/about.a376b287.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkzakat"]=self["webpackChunkzakat"]||[]).push([[594],{1417:function(t,n,u){u.r(n),u.d(n,{default:function(){return i}});var a=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,n=t._self._c;return n("div",{staticClass:"about"},[n("h1",[t._v("This is an about page")])])}],e=u(1656),l={},r=(0,e.A)(l,a,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.a376b287.js.map
